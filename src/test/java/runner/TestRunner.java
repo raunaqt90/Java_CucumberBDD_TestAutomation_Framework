5f3d92945c36e800e5142c6dcd94c9f3165756b2
raunaqt90 <raunaq90@ymail.com>
2023-11-08 19:22:35 -0500

    Added reports

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,10 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/resources/features",
         glue = {"org.opencart.stepdefs"},
-        plugin = {"pretty","html:target/cucumber-reports", "json:target/cucumber.json"},
+        //plugin = {"pretty","html:target/cucumber-reports", "json:target/cucumber.json"},
+        plugin = {"pretty",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
         monochrome = true,
         publish=true)
 
